c9354dc042fff31198261a99fce71a7aad19438f
Shinevv27 <90701366+Shinevv27@users.noreply.github.com>
2024-01-06 06:51:23 +0700

    Delete poliklinik_bk directory

--- poliklinik_bk/Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua variabel sesi
-session_destroy(); // Menghancurkan sesi
-header("Location: loginUser.php"); // Redirect ke halaman login atau halaman lain setelah logout
-exit();
-?>
-
